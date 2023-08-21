63c230b666c3c6c2736d638a984e95a489c0584c
Ajinkya <ajoshi@viewpoint.ai>
2023-08-21 14:50:07 -0700

    weather-app

---
+++ app.js
@@ -0,0 +1,53 @@
+// MODULE
+var weatherApp = angular.module('weatherApp', ['ngRoute', 'ngResource']);
+
+// ROUTES
+weatherApp.config(function ($routeProvider) {
+   
+    $routeProvider
+    
+    .when('/', {
+        templateUrl: 'pages/home.htm',
+        controller: 'homeController'
+    })
+    
+    .when('/forecast', {
+        templateUrl: 'pages/forecast.htm',
+        controller: 'forecastController'
+    })
+    
+    .when('/forecast/:days', {
+        templateUrl: 'pages/forecast.htm',
+        controller: 'forecastController'
+    })
+    
+});
+
+// SERVICES
+weatherApp.service('cityService', function() {
+   
+    this.city = "New York, NY";
+    
+});
+
+// CONTROLLERS
+weatherApp.controller('homeController', ['$scope', 'cityService', function($scope, cityService) {
+    
+    $scope.city = cityService.city;
+    
+    $scope.$watch('city', function() {
+       cityService.city = $scope.city; 
+    });
+    
+}]);
+
+weatherApp.controller('forecastController', ['$scope', '$resource', '$routeParams', 'cityService', function($scope, $resource, $routeParams, cityService) {
+    
+    $scope.city = cityService.city;
+    
+    $scope.days = $routeParams.days || '2';
+    $scope.weatherAPI = $resource("http://api.weatherapi.com/v1/forecast.json?key=9e00b8d4309c48f6b81213044232008", { callback: "JSON_CALLBACK" }, + '&'+ { get: { method: "JSONP" }});
+    
+    $scope.weatherResult = $scope.weatherAPI.get({ q: $scope.city, days: $scope.days });
+    
+}]);
